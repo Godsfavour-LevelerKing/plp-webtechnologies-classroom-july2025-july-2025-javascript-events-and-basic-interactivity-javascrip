5ad1f6bde1cae72c8b81a57d8d96682d9d3db82d
Godsfavour-LevelerKing <roetechhub@gmail.com>
2025-09-09 06:08:23 -0700

    Initial Commit: My Week6 webdev assignment

---
+++ script.js
@@ -0,0 +1,61 @@
+// Part 1
+document.getElementById('msgBtn').addEventListener("click", ()=>{
+    document.getElementById('output').textContent = "You Clicked the Button";
+});
+
+// part 2
+const toggleBtn = document.getElementById("toggleTheme");
+toggleBtn.addEventListener("click", ()=>{
+    document.body.classList.toggle("darkmode");
+});
+
+// my count game
+let count = 0;
+document.getElementById("countBtn").addEventListener("click",()=>{
+    count ++;
+    document.getElementById("countDisplay").textContent = count;
+} );
+
+
+// part 3
+
+document.getElementById("signupForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+    let msg = document.getElementById("formMsg");
+
+    // here i checked if the form is empty
+
+    if (name === "" || email === "" || password === ""){
+        msg.textContent = "All fields are required.";
+        msg.style.color = "red";
+        return;
+    }
+
+    // here i set the email format, but it's giving me issues 🥲😭
+
+    // let emailPattern = /^[^ ]+@[^ ]+\[a-z]{2,3}$/;
+
+    // if (!email.match(emailPattern)) {
+    //     msg.textContent = "Enter a valid Email Address";
+    //     msg.style.color = "red";
+    //     return;
+    // }
+
+
+    // here i set the password length
+    if(password.length < 6){
+        msg.textContent = "Password must be at least 6 chracters bro";
+        msg.style.color = "red";
+        return;
+    }
+
+    
+    // Submit success
+    msg.textContent = "Form is submitted Successfully";
+    msg.style.color = "green";
+
+})
